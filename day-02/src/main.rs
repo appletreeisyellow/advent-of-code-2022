6fd164e02bc2f8bf3e7b29eb54eb4e6e8d59a3cd
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-12-04 00:18:32 -0600

    chore: add day-02 solution for part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -2,10 +2,9 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader, Lines, Result};
 use std::path::Path;
 
-// my shapes
-const X: u32 = 1;
-const Y: u32 = 2;
-const Z: u32 = 3;
+const X: u32 = 1; // rock,
+const Y: u32 = 2; // paper
+const Z: u32 = 3; // scissor
 
 // scores
 const WIN: u32 = 6;
@@ -48,12 +47,46 @@ fn get_total_score(lines: Lines<BufReader<File>>) -> u32 {
         .sum()
 }
 
+fn get_total_score_2(lines: Lines<BufReader<File>>) -> u32 {
+    lines
+        .into_iter()
+        .filter_map(|line| {
+            if let Some(str) = line.ok() {
+                let opponent: String = str.clone().chars().nth(0).unwrap().to_string();
+                let result: String = str.chars().nth(2).unwrap().to_string();
+                match (&*opponent, &*result) {
+                    ("A", "X") => Some(Z + LOST), // LOST -> rock, scissor
+                    ("A", "Y") => Some(X + DRAW), // DRAW -> rock, rock
+                    ("A", "Z") => Some(Y + WIN),  // WIN -> rock, paper
+                    ("B", "X") => Some(X + LOST), // LOST -> paper, rock
+                    ("B", "Y") => Some(Y + DRAW), // DRAW -> paper, paper
+                    ("B", "Z") => Some(Z + WIN),  // WIN -> paper, scissor
+                    ("C", "X") => Some(Y + LOST), // LOST -> scissor, paper
+                    ("C", "Y") => Some(Z + DRAW), // DRAW -> scissor, scissor
+                    ("C", "Z") => Some(X + WIN),  // WIN -> scissor, rock
+                    (&_, _) => Some(0),
+                }
+            } else {
+                Some(0)
+            }
+        })
+        .sum()
+}
+
 fn main() {
+    // part 1
     if let Ok(lines) = read_lines("./example.txt") {
         assert_eq!(get_total_score(lines), 15);
     }
-
     if let Ok(lines) = read_lines("./input.txt") {
         println!("{:?}", get_total_score(lines));
+    }
+
+    // part 2
+    if let Ok(lines) = read_lines("./example.txt") {
+        assert_eq!(get_total_score_2(lines), 12);
+    }
+    if let Ok(lines) = read_lines("./input.txt") {
+        println!("{:?}", get_total_score_2(lines));
     }
 }
