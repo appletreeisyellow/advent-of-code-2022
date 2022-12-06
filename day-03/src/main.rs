a7d26e765a61719c90cbf9c5ee9f144ed75b7228
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-12-06 16:50:29 -0600

    refactor: use `include_str!()` macro instead of using `BufReader`

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,17 +1,4 @@
 use std::collections::HashSet;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader, Lines, Result};
-use std::path::Path;
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
 
 // Convert the ascii value in decimal to priority value
 //   - Lowercase item types a through z have priorities 1 through 26
@@ -51,37 +38,44 @@ fn find_common_char(str1: &str, str2: &str) -> char {
     }
 }
 
-fn get_priority_sum(lines: Lines<BufReader<File>>) -> u32 {
+fn get_priority_sum(lines: Vec<&str>) -> u32 {
     lines
         .into_iter()
         .filter_map(|line| {
-            if let Some(row) = line.ok() {
-                let mid = row.len() / 2;
-                let first_compartment = &row[..mid];
-                let second_compartment = &row[mid..];
-                let common_char = find_common_char(first_compartment, second_compartment);
-                Some(convert_to_priority(common_char as u32))
-            } else {
-                Some(0)
-            }
+            let mid = line.len() / 2;
+            let first_compartment = &line[..mid];
+            let second_compartment = &line[mid..];
+            let common_char = find_common_char(first_compartment, second_compartment);
+            Some(convert_to_priority(common_char as u32))
         })
         .sum::<u32>()
 }
 
-fn main() {
-    // test helper functions
-    assert_eq!(convert_to_priority('a' as u32), 1);
-    assert_eq!(convert_to_priority('b' as u32), 2);
-    assert_eq!(convert_to_priority('z' as u32), 26);
-    assert_eq!(convert_to_priority('A' as u32), 27);
-    assert_eq!(convert_to_priority('Z' as u32), 52);
-    assert_eq!(find_common_char("abc", "bdd"), 'b');
-    assert_eq!(find_common_char("abcds", "pesr"), 's');
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    if let Ok(lines) = read_lines("./example.txt") {
-        assert_eq!(get_priority_sum(lines), 157);
+    const EXAMPLE: &str = include_str!("../example.txt");
+
+    #[test]
+    fn test_helper_functions() {
+        assert_eq!(convert_to_priority('a' as u32), 1);
+        assert_eq!(convert_to_priority('b' as u32), 2);
+        assert_eq!(convert_to_priority('z' as u32), 26);
+        assert_eq!(convert_to_priority('A' as u32), 27);
+        assert_eq!(convert_to_priority('Z' as u32), 52);
+        assert_eq!(find_common_char("abc", "bdd"), 'b');
+        assert_eq!(find_common_char("abcds", "pesr"), 's');
     }
-    if let Ok(lines) = read_lines("./input.txt") {
-        println!("{:?}", get_priority_sum(lines));
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(get_priority_sum(EXAMPLE.lines().collect()), 157);
     }
+}
+
+fn main() {
+    let input: Vec<&str> = include_str!("../input.txt").lines().collect();
+
+    println!("Part 1: {:?}", get_priority_sum(input));
 }
