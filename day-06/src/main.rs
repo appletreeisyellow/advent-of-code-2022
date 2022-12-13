32e49431067ebca8e3e8ad40dc7926e4fcce1edf
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-12-12 23:27:33 -0600

    feat: add day-06 solution for part 1

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,46 @@
+use std::collections::{HashSet, VecDeque};
+
+fn get_first_marker(str: &str) -> usize {
+    let mut queue: VecDeque<char> = VecDeque::new();
+
+    if let Some(idx) = str.chars().enumerate().find_map(|(i, c): (usize, char)| {
+        if queue.len() < 4 {
+            queue.push_back(c);
+            None
+        } else {
+            queue.pop_front();
+            queue.push_back(c);
+
+            let r: HashSet<char> = queue.clone().into_iter().collect::<HashSet<char>>();
+            if r.len() == 4 {
+                Some(i)
+            } else {
+                None
+            }
+        }
+    }) {
+        idx + 1
+    } else {
+        0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(get_first_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(get_first_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(get_first_marker("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(get_first_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(get_first_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input: Vec<&str> = include_str!("../input.txt").lines().collect();
+
+    println!("Part 1: {:?}", get_first_marker(input.get(0).unwrap()));
 }
